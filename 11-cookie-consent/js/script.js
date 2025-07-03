b0113b78e3b550bf8f713ab4b5dd044ae2bc58f5
karlmarxlive <savenko.egor42@gmail.com>
2025-07-03 14:22:13 +0700

    fix(11): attach listeners only if popup wasn't removed

--- 11-cookie-consent/js/script.js
+++ 11-cookie-consent/js/script.js
@@ -1,16 +1,19 @@
-const cookiesCloseBtn = document.querySelector(".cookies__close-btn");
-const cookiesConfirmBtn = document.querySelector(".cookies__confirm-btn");
 const cookiesWindow = document.querySelector(".cookies");
 
 if (localStorage.getItem('cookiesConfirmed')) {
     cookiesWindow.remove();
+} else {
+    const cookiesCloseBtn = document.querySelector(".cookies__close-btn");
+    const cookiesConfirmBtn = document.querySelector(".cookies__confirm-btn");
+
+    cookiesCloseBtn.addEventListener("click", () => {
+        cookiesWindow.remove();
+    });
+
+    cookiesConfirmBtn.addEventListener("click", () => {
+        localStorage.setItem('cookiesConfirmed', true);
+        cookiesWindow.remove();
+    });
 }
 
-cookiesCloseBtn.addEventListener("click", () => {
-    cookiesWindow.remove();
-});
 
-cookiesConfirmBtn.addEventListener("click", () => {
-    localStorage.setItem('cookiesConfirmed', true);
-    cookiesWindow.remove();
-});
