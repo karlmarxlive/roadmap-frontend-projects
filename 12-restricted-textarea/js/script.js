f7423c8b8730da5395e1a018f91fb0d231995b0c
karlmarxlive <savenko.egor42@gmail.com>
2025-07-03 20:42:50 +0700

    feat(12): add textarea character limit handle

--- 12-restricted-textarea/js/script.js
+++ 12-restricted-textarea/js/script.js
@@ -1,2 +1,22 @@
-const textarea = document.querySelector('.textarea');
-const counter = document.querySelector('.textarea__counter');
+const input = document.querySelector('.message__input');
+const counter = document.querySelector('.message__counter');
+const characterLimit = input.maxLength;
+
+function updateTextarea() {
+    const len = input.value.length;
+    counter.textContent = `${len}/${characterLimit}`;
+
+    const limitReached = len >= characterLimit;
+    input.classList.toggle('message__input--limit', limitReached);
+    counter.classList.toggle('message__counter--limit', limitReached);
+};
+
+input.addEventListener('beforeinput', (e) => {
+    if (input.value.length >= characterLimit && e.inputType.startsWith('insert')) {
+        e.preventDefault();
+    }
+});
+
+input.addEventListener('input', updateTextarea);
+
+updateTextarea();
